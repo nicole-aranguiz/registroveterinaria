123775b2a5dac685d3d8271ccf5b8698b957dd7b
Nicole <aranguiznicolem@gmail.com>
2026-07-08 22:30:40 -0400

    Implementación de const

---
+++ script.js
@@ -0,0 +1,7 @@
+let mascotas = [];
+
+// Selección de elementos
+const formMascota = document.getElementById('formMascota');
+const mensajeError = document.getElementById('mensajeError');
+const listaMascotas = document.getElementById('listaMascotas');
+
